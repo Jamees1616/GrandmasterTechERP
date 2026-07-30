e38e1ab53f968a2a560272b4bb36ac267d5ef706
James1616 <jamees1616@gmail.com>
2026-07-30 19:57:20 +0200

    Initialize database for production deployment

--- app.py
+++ app.py
@@ -309,7 +309,11 @@ def knowledge():
 
 # ========== INIT ==========
 
-if __name__ == '__main__':
+def initialize_database():
     with app.app_context():
         db.create_all()
+
+initialize_database()
+
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
